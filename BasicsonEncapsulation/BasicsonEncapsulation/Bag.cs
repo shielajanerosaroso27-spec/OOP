b84e9ddaa9a5a60018e64075015c8a2686776caa
shielajanerosaroso27-spec <shielajanerosaroso27@gmail.com>
2025-08-29 14:22:28 +0800

    Add files via upload

---
+++ BasicsonEncapsulation/BasicsonEncapsulation/Bag.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsonEncapsulation
+{
+    internal class Bag
+    {
+        //
+        private string brand;
+        private string color;
+
+        //
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        //Method to display the Bag's Information
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {brand}\n Color:{Color}");
+        }
+    }
+}
---
+++ BasicsonEncapsulation/BasicsonEncapsulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsonEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SHIELA JANE ROSAROSO
+            //IT306
+            //Basics on Encapsulation
+
+            //Instantiate Object
+            Bag bag1 = new Bag();
+
+            //Set the properties
+            Console.WriteLine("Bag's Information:");
+            bag1.Brand = "Gucci";
+            bag1.Color = "Pink";
+
+            //Method to display the object details
+            bag1.DisplayInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
