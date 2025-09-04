1022d5cdbdaede161db2a9817f733638bb7e97f7
shielajanerosaroso27-spec <shielajanerosaroso27@gmail.com>
2025-09-04 13:48:07 +0800

    Add files via upload

---
+++ Rosaroso_SimpleInheritance/Rosaroso_SimpleInheritance/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosaroso_SimpleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Rosaroso, Shiela Jane B.
+            //IT306
+            //Simple Inheritance
+
+            //Create an instance of the Student class
+            Student student = new Student
+            {
+                Name = "Keishi Mae Alaan" , 
+                Program = "BSIT"
+             };
+
+            //call the method of the Student Class
+            student.BasicInfo();// Call Method to display
+
+            //Instantiate the object of the Regular Student 
+            RegularStudent regularStudent = new RegularStudent
+            {
+                Name = "Keisha Baril",
+                Program = "BSHM",
+                Section = "IT306"
+            };
+            //call the method of the Regular Student 
+            regularStudent.BasicInfo();
+            regularStudent.SectionEnrolled();
+
+            IrregularStudent irregularStudent = new IrregularStudent
+            {
+                Name = "Sheila Mei Rosaroso",
+                Program = "BSN",
+                UnitsEnrolled = 18
+            };
+            //call the method of the Irregular Student 
+            irregularStudent.BasicInfo();
+            irregularStudent.EnrolledSemUnits();
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Rosaroso_SimpleInheritance/Rosaroso_SimpleInheritance/Stduent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosaroso_SimpleInheritance
+{
+    internal class Student //Base Class
+    {
+        //Properties for the Student Class
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        //Method to display Student Information
+        public void BasicInfo()
+        {
+            Console.WriteLine("*** Student Information ***");
+            Console.WriteLine($"Name: {Name} , \n Program: {Program}");
+        }
+    }
+    class RegularStudent : Student //Derived class
+    {
+        //Additional property for the Student Class
+        public string Section { get; set; }
+
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"Section: {Section}");
+        }
+    }
+
+    class IrregularStudent : Student //Derived class
+    {
+        public int UnitsEnrolled { get; set; }
+
+        public void EnrolledSemUnits() 
+        {
+            Console.WriteLine($"UnitsEnrolled: {UnitsEnrolled}");
+        }
+    }
+}
