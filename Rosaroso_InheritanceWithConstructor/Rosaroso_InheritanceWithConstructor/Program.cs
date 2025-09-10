177c5c249e750851d3a1188db469281c2650f5f9
shielajanerosaroso27-spec <shielajanerosaroso27@gmail.com>
2025-09-10 14:53:40 +0800

    Add files via upload

---
+++ Rosaroso_InheritanceWithConstructor/Rosaroso_InheritanceWithConstructor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosaroso_InheritanceWithConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shiela Jane Rosaroso
+            //IT306
+            //Simple Inheritance with Constructor
+
+            //Instantiate Student Object
+            Student student = new Student ("Shiela Jane Rosaroso", "IT306");
+
+            //Student Display
+            Console.WriteLine("\n********** STUDENT INFORMATION **********");
+            student.BasicInfo();
+            Console.WriteLine("*********************************************");
+
+            //Instantiate Regular Student Class
+            RegularStudent regularStudent = new RegularStudent ("Keisha", "BSHM", "IT306");
+
+            //Regular Student Display
+            Console.WriteLine("\n ********** REGULAR STUDENT INFORMATION ********** ");
+            regularStudent.BasicInfo();
+            regularStudent.SectionEnrolled();
+            Console.WriteLine("*********************************************");
+
+
+            //Instantiate Irregular Student 
+            IrregularStudent irregularStudent = new IrregularStudent("Xania", "BSTM", "IT306", 23);
+
+            //Irregular Student Display
+            Console.WriteLine("\n ********** IRREGULAR STUDENT INFORMATION ********** ");
+            irregularStudent.BasicInfo();
+            irregularStudent.EnrolledSemUnits();
+            Console.WriteLine("*********************************************");
+             
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rosaroso_InheritanceWithConstructor/Rosaroso_InheritanceWithConstructor/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosaroso_InheritanceWithConstructor
+{
+        public class Student //Base class
+        {
+            string name;
+            string program;
+
+            public Student (string Name, string Program) //Properties of the student class
+            {
+                name = Name;
+                program = Program;
+            }
+
+            public void BasicInfo()
+            {
+                Console.WriteLine(" Name: " + name);
+                Console.WriteLine(" Program: " + program);
+            }
+        }
+
+       public class RegularStudent : Student //Derived Class
+    {
+        public string section;
+
+        public RegularStudent(string Name, string Program , string Section) : base(Name, Program)
+        {
+            section = Section;
+        }
+        public void SectionEnrolled() //Method for the Regular Student
+        {
+            Console.WriteLine("Section:  " + section);
+        }
+
+    }
+
+    public class IrregularStudent : Student //Another Derived Class
+    {
+        int unitsEnrolled;
+        public IrregularStudent(string Name, string Program, string Section, int UnitsEnrolled) : base(Name, Program)
+        {
+            unitsEnrolled = UnitsEnrolled;
+        }
+
+        public void EnrolledSemUnits() //Method to display Irregular Student
+        {
+            Console.WriteLine("Units Enrolled:   " + unitsEnrolled);
+        }
+    }
+
+}
